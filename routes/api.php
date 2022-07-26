d1339e4a65b1c59178274bb538d55a87c69e102d
BrenaKellyane <brenakellyane53@gmail.com>
2022-07-26 08:26:15 -0300

    Corigindo as rotas da api

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('links', LinkController::class);
+//Route::get('/link', '\App\Http\Controllers\LinkController@index');
 
-Route::get('/redirect/{codigo}', [LinkCurtoController::class, 'redirecionamento']);
-Route::resource('links-curtos', LinkCurtoController::class);
+Route::get('/links', [LinkController::class, 'index'])->name('links.index');
+Route::post('/links/store', [LinkController::class, 'store'])->name('links.store');
+Route::post('/links/destroy', [LinkController::class, 'destroy'])->name('links.destroy');
+
+Route::get('/links-curtos', [LinkCurtoController::class, 'index'])->name('links_curtos.index');
+Route::get('/links-curtos/store', [LinkCurtoController::class, 'store'])->name('links_curtos.store');
+Route::get('/links-curtos/destroy', [LinkCurtoController::class, 'destroy'])->name('links_curtos.destroy');
+Route::get('/links-curtos/destroy', [LinkCurtoController::class, 'destroy'])->name('links_curtos.destroy');
+Route::get('/redirect/{codigo}', [LinkCurtoController::class, 'redirecionamento'])->name('links_curtos.redirect');
